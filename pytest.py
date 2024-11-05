953f2d0682f03589483a31183432b01e5921cd58
Lucas <lucasjaskowiak@yahoo.fr>
2024-11-05 16:24:26 +0100

    création du quadrillage

---
+++ pytest.py
@@ -0,0 +1,123 @@
+import pygame
+from pygame.locals import *
+import time
+
+pygame.init()
+screen = pygame.display.set_mode((1720, 980))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+world_y = screen.get_height()
+
+
+class World:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.sky = 20
+        self.pattern = [
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0] * 100,
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ]
+        self.height = screen.get_height()
+        self.width = screen.get_width()
+        self.rectsY = [""]
+
+    def draw(self):
+        for y in range(len(self.pattern)):
+            for x in range(len(self.pattern[y])):
+                rect = pygame.Rect(x*30, y*30, 30, 30)
+                pygame.draw.rect(screen, "red", rect, 1)
+                self.rectsY += [rect.y]
+                if self.pattern[y][x]:
+                    pygame.draw.rect(screen, "red", rect)
+
+class Character:
+
+    def __init__(self, image, x, y):
+        self.image = pygame.image.load(image)
+        self.height = self.image.get_height()
+        self.x = x
+        self.y = y
+        self.can_jump = False
+        self.vel_y = 1.00001
+        self.posX = self.x
+
+    def draw(self):
+        screen.blit(self.image, (self.x, self.y))
+
+    def gravity(self):
+        time.sleep(0.001)
+        self.y += self.vel_y
+        if int(self.y + self.height) >= world.rectsY:
+            self.y = world_y - self.height
+            self.can_jump = True
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.can_jump:
+            self.can_jump = False
+            self.y -= 20
+        if keys[pygame.K_q]:
+            self.x -= 600 * dt
+        if keys[pygame.K_d]:
+            self.x += 600 * dt
+
+player = Character("assets/img/Ark.gif", 100, 100)
+world = World()
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.update()
+    
+    screen.fill("green")
+
+
+    world.draw()
+
+    player.draw()
+    player.vel_y += player.vel_y
+    player.gravity()
+    player.move()
+
+    clock.tick(60) 
+    dt = clock.tick(60) / 1000
+pygame.quit()
+
+
